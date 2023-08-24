6939b078749bda20e2a90613195ee4a04671ce36
v0idpwn <v0idpwn@gmail.com>
2023-08-24 11:19:38 +0300

    Add deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Pgmq.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -21,8 +22,11 @@ defmodule Pgmq.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.10"},
+      {:jason, "~> 1.0", only: :test}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
