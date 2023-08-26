8b053aeebb57c40d935b512ab876b65fada356a5
v0idpwn <v0idpwn@gmail.com>
2023-08-26 23:43:14 +0300

    Docs and formatting

--- lib/pgmq/message.ex
+++ lib/pgmq/message.ex
@@ -1,7 +1,26 @@
 defmodule Pgmq.Message do
-  @enforce_keys [:id, :read_count, :enqueued_at, :visibility_timeout, :body]
-  defstruct [:id, :read_count, :enqueued_at, :visibility_timeout, :body]
+  @moduledoc """
+  A message read from pgmq
+  """
+  @enforce_keys [
+    :id,
+    :read_count,
+    :enqueued_at,
+    :visibility_timeout,
+    :body
+  ]
 
+  defstruct [
+    :id,
+    :read_count,
+    :enqueued_at,
+    :visibility_timeout,
+    :body
+  ]
+
+  @typedoc """
+  A message read from pgmq
+  """
   @type t :: %__MODULE__{
           id: integer,
           body: binary,
@@ -10,7 +29,13 @@ defmodule Pgmq.Message do
           visibility_timeout: Date.t()
         }
 
-  def from_row([id, read_count, enqueued_at, visibility_timeout, body]) do
+  def from_row([
+        id,
+        read_count,
+        enqueued_at,
+        visibility_timeout,
+        body
+      ]) do
     %__MODULE__{
       id: id,
       read_count: read_count,
